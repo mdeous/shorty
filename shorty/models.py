63c0e0c02da52ef9bdf7d67c6f24bf4c7b05720c
MatToufoutu <mattoufootu@gmail.com>
2011-10-01 14:00:19 +0200

    remove the ShortURL.url_code column, we don't need it

--- shorty/models.py
+++ shorty/models.py
@@ -37,7 +37,7 @@ class AutoInitModelMixin(object):
 
 class ShortURL(db.Model, AutoInitModelMixin):
     long_url = db.Column(db.String(255), unique=True)
-    url_code = db.Column(db.String(32), unique=True) #XXX: is this really needed?
+    #url_code = db.Column(db.String(32), unique=True) #XXX: is this really needed?
     created = db.Column(db.DateTime, default=datetime.now)
     clicks = db.relationship("Click")
 
