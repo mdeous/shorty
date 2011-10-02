b54b00ccae6aa38dd46b4fe3e648acc10bf344c3
MatToufoutu <mattoufootu@gmail.com>
2011-10-02 13:11:36 +0200

    make core.setup_routing code less compact (readability counts)

--- shorty/core/__init__.py
+++ shorty/core/__init__.py
@@ -16,9 +16,13 @@ def setup_routing(app, routes):
     :returns: None
     """
     for route in routes:
+        # endpoint: (blueprint_instance, url_prefix)
+        # rules: [('/route/', view_function), ...]
         endpoint, rules = route[0], route[1:]
         for pattern, view in rules:
-            (app if endpoint is None else endpoint[0]
-                ).add_url_rule(pattern, view_func=view)
+            if endpoint is None:
+                app.add_url_rule(pattern, view_func=view)
+            else:
+                endpoint[0].add_url_rule(pattern, view_func=view)
         if endpoint is not None:
             app.register_blueprint(endpoint[0], url_prefix=endpoint[1])
