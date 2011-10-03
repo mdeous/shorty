279498cd9415f266b1251b796e4999012f3e9759
MatToufoutu <mattoufootu@gmail.com>
2011-10-03 14:05:40 +0200

    expose flask-wtf settings in the settings.py file

--- shorty/settings.py
+++ shorty/settings.py
@@ -5,6 +5,8 @@ TESTING = False
 
 SECRET_KEY = 'DuMmY sEcReT kEy'
 SESSION_COOKIE_SECURE = True
+CSRF_ENABLED = True
+CSRF_SESSION_KEY = '_csrf_token'
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///shorty.sqlite'
 
