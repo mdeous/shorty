bcd7dab788e093708c9e9be5cf9ee950cd3cca68
MatToufoutu <mattoufootu@gmail.com>
2011-10-08 20:17:46 +0200

    fix typo in context processors

--- shorty/context_processors.py
+++ shorty/context_processors.py
@@ -4,6 +4,6 @@ def static_files():
     STATIC_ROOT = '/static'
     return dict(
         STATIC_ROOT = STATIC_ROOT,
-        CSS = '%s/css' % STATIC,
-        JS = '%s/js' % STATIC,
+        CSS = '%s/css' % STATIC_ROOT,
+        JS = '%s/js' % STATIC_ROOT,
     )
