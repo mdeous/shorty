e42d2e19f99cdee0add498c1fa6f8f68bdf66082
MatToufoutu <mattoufootu@gmail.com>
2011-10-08 19:52:06 +0200

    shorten static files related template context variables (remove _PATH prefix)

--- shorty/context_processors.py
+++ shorty/context_processors.py
@@ -4,6 +4,6 @@ def static_files():
     STATIC_ROOT = '/static'
     return dict(
         STATIC_ROOT = STATIC_ROOT,
-        CSS_PATH = '%s/css' % STATIC_ROOT,
-        JS_PATH = '%s/js' % STATIC_ROOT,
+        CSS = '%s/css' % STATIC,
+        JS = '%s/js' % STATIC,
     )
