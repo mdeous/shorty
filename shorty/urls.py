3b7afdb91685434728f85ad49537e4f1f5409fa2
MatToufoutu <mattoufootu@gmail.com>
2011-10-09 12:34:54 +0200

    write the shortener views

--- shorty/urls.py
+++ shorty/urls.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from shorty.views.frontend import frontend, IndexView
+from shorty.views.frontend import frontend, IndexView, ShortLinkRedirectView
 
 routes = [
     ((frontend, ''),
         ('/', IndexView.as_view('index')),
+        ('/<short_code>', ShortLinkRedirectView.as_view('redir')),
     ),
 ]
--- shorty/views/frontend.py
+++ shorty/views/frontend.py
@@ -1,11 +1,37 @@
 # -*- coding: utf-8 -*-
 
 from flask import *
-from flask.views import MethodView
+from flask.views import MethodView, View
+
+from shorty.core.shortener import shorten_url, expand_url
+from shorty.forms import URLForm
 
 frontend = Blueprint('frontend', __name__)
 
 
 class IndexView(MethodView):
+    template = 'index.html'
+
     def get(self):
-        return render_template('index.html')
+        form = URLForm()
+        return render_template(self.template,
+                               form=form)
+
+    def post(self):
+        form = URLForm()
+        if not form.validate_on_submit():
+            return render_template(self.template,
+                                   form=form)
+        short_code = shorten_url(form.data['url'])
+        short_url = '%s/%s' % (current_app.config['BASE_URL'], short_code)
+        return render_template(self.template,
+                               form=form,
+                               short_url=short_url)
+
+
+class ShortLinkRedirectView(View):
+    methods = ['GET']
+
+    def dispatch_request(self, short_code):
+        long_url = expand_url(short_code)
+        return redirect(long_url)
