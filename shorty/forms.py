053a323861ddb445c522743d8e5be1e4bbcbf2aa
MatToufoutu <mattoufootu@gmail.com>
2011-10-09 12:06:45 +0200

    add a SubmitField to ShortenForm

--- shorty/forms.py
+++ shorty/forms.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from flask.ext.wtf import Form, TextField
+from flask.ext.wtf import Form, TextField, SubmitField
 from flask.ext.wtf import Required, Length, URL
 
 
 class ShortenForm(Form):
     url = TextField('URL', [Required(), Length(max=255), URL()])
-
+    submit = SubmitField('shorten')
